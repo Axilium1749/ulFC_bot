447e08fd10a2d56fd6088e1aa2df3558f1a77170
Axilium1749 <lordragnamauyt@gmail.com>
2026-07-15 16:24:47 +0300

    bugfix

--- bot/database/db.py
+++ bot/database/db.py
@@ -2,7 +2,10 @@ from datetime import datetime, timezone
 from sqlalchemy import BigInteger, String, DateTime, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
+import sys, os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
 from config import DATABASE_URL
 
 # Подключаемся к базе, которую мы создали в докере
